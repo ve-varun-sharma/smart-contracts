1d31931acafed573859ee306d808444ded821971
Varun Sharma <varun_sharma@live.co.uk>
2024-12-22 23:24:52 -0500

    fix: deploymeny script for btc oracle

--- oracles/src/deployment-scripts/deployBtcPriceOracle.js
+++ oracles/src/deployment-scripts/deployBtcPriceOracle.js
@@ -1,13 +1,9 @@
-const { ethers } = require('ethers')
 const hardhat = require('hardhat')
 
 async function main() {
   const [deployer] = await hardhat.ethers.getSigners()
 
   console.log('Deploying contracts with the account:', deployer.address)
-
-  const balance = await deployer.getBalance()
-  console.log('Account balance:', balance.toString())
 
   const BitcoinPrice = await hardhat.ethers.getContractFactory('BitcoinPrice')
   const bitcoinPrice = await BitcoinPrice.deploy()
