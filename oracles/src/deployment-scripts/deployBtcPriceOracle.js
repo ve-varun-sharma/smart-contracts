18d0371607de937df9a9e6b8fb899011e8249ef8
Varun Sharma <varun_sharma@live.co.uk>
2024-12-23 00:15:21 -0500

    feat: make btc oracle deployment script more robust

--- oracles/src/deployment-scripts/deployBtcPriceOracle.js
+++ oracles/src/deployment-scripts/deployBtcPriceOracle.js
@@ -1,14 +1,57 @@
 const hardhat = require('hardhat')
+const readline = require('readline')
 
+function askUser(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  return new Promise(resolve => {
+    rl.question(question, answer => {
+      rl.close()
+      resolve(answer)
+    })
+  })
+}
 async function main() {
   const [deployer] = await hardhat.ethers.getSigners()
 
   console.log('Deploying contracts with the account:', deployer.address)
+  const BitcoinPriceOracle = await hardhat.ethers.getContractFactory('BitcoinPrice')
 
-  const BitcoinPrice = await hardhat.ethers.getContractFactory('BitcoinPrice')
-  const bitcoinPrice = await BitcoinPrice.deploy()
+  // const balance = await deployer.getBalance();
+  // console.log('Account balance:', hre.ethers.utils.formatEther(balance), 'ETH');
 
-  console.log('BitcoinPrice contract deployed to:', bitcoinPrice.address)
+  // Prepare the deploy transaction
+  const deployTxRequest = BitcoinPriceOracle.getDeployTransaction()
+
+  // Estimate gas and cost
+  // Instead of deployer.estimateGas(...), directly use hre.ethers.provider.estimateGas
+  const estimatedGas = await hre.ethers.provider.estimateGas({
+    ...deployTxRequest,
+    from: deployer.address,
+  })
+
+  // Both estimatedGas and gasPrice should be BigNumbers
+  console.log(`Estimated gas cost: = ${estimatedGas.toString()}`)
+
+  // Prompt the user to proceed or reject
+  const terminalRl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  // Ask user for confirmation
+  const answer = await askUser('Do you want to proceed with the deployment? (yes/no): ')
+  if (answer.toLowerCase() === 'yes') {
+    const bitcoinPrice = await BitcoinPriceOracle.deploy()
+    await bitcoinPrice.deployed() // Wait for the deployment to be mined
+
+    console.log('BitcoinPrice contract deployed to:', bitcoinPrice.address)
+  } else {
+    console.log('Deployment cancelled.')
+  }
+  terminalRl.close()
 }
 
 main()
