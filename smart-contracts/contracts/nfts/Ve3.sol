03345061df34f32d2729c1389112a0d954449c5d
Varun Sharma <62218708+ve-varun-sharma@users.noreply.github.com>
2025-01-03 22:20:52 -0800

    feat: make improvemetns to security of the contract

--- smart-contracts/contracts/nfts/Ve3.sol
+++ smart-contracts/contracts/nfts/Ve3.sol
@@ -5,22 +5,27 @@ import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
 
-contract Ve3NFT is ERC721URIStorage {
+contract Ve3NFT is ERC721URIStorage, Ownable {
   using Strings for uint256;
 
   uint256 private _tokenIds;
   string private _baseTokenURI;
 
-  constructor() ERC721('Ve3NFT', 'VE3') {
-    _baseTokenURI = 'https://storage.googleapis.com/your-bucket/metadata/';
+  // emit event for transparency of base uri changing
+  event BaseURIChanged(string newBaseURI);
+
+  constructor() ERC721('Ve3NFT', 'VE3') Ownable(msg.sender) {
+    _baseTokenURI = 'https://storage.googleapis.com/web3-nfts/ve-nfts/vesharma.dev%20%20QR%20code.png';
   }
 
   function _baseURI() internal view virtual override returns (string memory) {
     return _baseTokenURI;
   }
 
-  function setBaseURI(string memory baseURI) public {
+  function setBaseURI(string memory baseURI) public onlyOwner {
+    // only the owner can set the base uri to prevent security issues
     _baseTokenURI = baseURI;
+    emit BaseURIChanged(baseURI);
   }
 
   function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
